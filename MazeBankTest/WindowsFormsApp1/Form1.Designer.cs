36d7e183d453de2a820b4e4cbfbf83748bb0bd36
Paul Stöger <paul.stoeger@hak-kitz.ac.at>
2017-10-27 10:04:54 +0200

    test button

--- MazeBankTest/WindowsFormsApp1/Form1.Designer.cs
+++ MazeBankTest/WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(46, 83);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(208, 128);
+            this.btn_test.TabIndex = 0;
+            this.btn_test.Text = "TEST";
+            this.btn_test.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_test);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_test;
     }
 }
 
